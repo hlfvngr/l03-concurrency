34a8ed75aaf53d01c20a50ca8026ed0867825b51
hlfvngr <45568428+hlfvngr@users.noreply.github.com>
2025-11-06 16:12:10 +0800

    feat: Implement matrix multiplication using multithreading

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,7 +1,11 @@
 use std::{
     fmt::{Debug, Display},
     ops::{Add, AddAssign, Mul},
+    sync::mpsc::{self, Sender},
+    thread,
 };
+
+const NUM_THREADS: usize = 4;
 // [1,2, 3,4, 5,6]
 pub struct Matrix<T> {
     data: Vec<T>,
@@ -16,6 +20,40 @@ impl<T> Matrix<T> {
             rows,
             cols,
         }
+    }
+}
+
+pub struct Msg<T> {
+    input: MsgInput<T>,
+    sender: oneshot::Sender<MsgOutput<T>>,
+}
+
+impl<T> Msg<T> {
+    pub fn new(input: MsgInput<T>, sender: oneshot::Sender<MsgOutput<T>>) -> Self {
+        Msg { input, sender }
+    }
+}
+
+pub struct MsgInput<T> {
+    idx: usize,
+    a: Vec<T>,
+    b: Vec<T>,
+}
+
+impl<T> MsgInput<T> {
+    pub fn new(idx: usize, a: Vec<T>, b: Vec<T>) -> Self {
+        MsgInput { idx, a, b }
+    }
+}
+
+pub struct MsgOutput<T> {
+    idx: usize,
+    result: T,
+}
+
+impl<T> MsgOutput<T> {
+    pub fn new(idx: usize, result: T) -> Self {
+        MsgOutput { idx, result }
     }
 }
 
@@ -71,6 +109,61 @@ where
     Matrix::new(data, a.rows, b.cols)
 }
 
+pub fn multiply_multi<T>(a: &Matrix<T>, b: &Matrix<T>) -> Matrix<T>
+where
+    T: Copy + Mul<Output = T> + AddAssign + Debug + Default + Add<Output = T> + Send + 'static,
+{
+    assert_eq!(a.rows, b.cols);
+
+    let senders = (0..NUM_THREADS)
+        .into_iter()
+        .map(|_| {
+            let (tx, rx) = mpsc::channel::<Msg<T>>();
+            thread::spawn(move || {
+                for msg in rx {
+                    let res = dot_product(&msg.input.a, &msg.input.b);
+                    msg.sender
+                        .send(MsgOutput::new(msg.input.idx, res))
+                        .expect("return result message fail!");
+                }
+            });
+            tx
+        })
+        .collect::<Vec<Sender<Msg<T>>>>();
+
+    let mut receivers = Vec::new();
+    let mut data = vec![T::default(); a.rows * b.cols];
+    for i in 0..a.rows {
+        for j in 0..b.cols {
+            let m = a.data[i * a.cols..(i + 1) * a.cols].to_vec();
+            let n = b.data[j..]
+                .iter()
+                .step_by(b.cols)
+                .map(|x| x.clone())
+                .collect::<Vec<T>>();
+
+            let idx = i * a.rows + j;
+
+            let (tx, rx) = oneshot::channel();
+            let msg_input = MsgInput::new(idx, m, n);
+            let msg = Msg::new(msg_input, tx);
+
+            senders[idx % NUM_THREADS]
+                .send(msg)
+                .expect("send calculation message fail!");
+
+            receivers.push(rx);
+        }
+
+        for r in receivers.iter() {
+            if let Ok(a) = r.recv_ref() {
+                data[a.idx] = a.result;
+            }
+        }
+    }
+    Matrix::new(data, a.rows, b.cols)
+}
+
 impl<T> Display for Matrix<T>
 where
     T: Display,
@@ -110,6 +203,14 @@ mod tests {
         let a = Matrix::new([1, 2, 3, 4, 5, 6], 2, 3);
         let b = Matrix::new([1, 2, 3, 4, 5, 6], 3, 2);
         let c = multiply(&a, &b);
+        println!("{c}")
+    }
+
+    #[test]
+    fn matrix_multiply_multi() {
+        let a = Matrix::new([1, 2, 3, 4, 5, 6], 2, 3);
+        let b = Matrix::new([1, 2, 3, 4, 5, 6], 3, 2);
+        let c = multiply_multi(&a, &b);
         println!("{c}")
     }
 }
